d89c2fb0d44232069ff0c724cc53ea73502b9d8d
DESKTOP-UN092U6\Mic23 <234976@student.pwr.edu.pl>
2021-05-24 18:03:20 +0200

    Early jwt implementation

--- Project_ZIwG.Domain/Auth/Interfaces/IAuthenticator.cs
+++ Project_ZIwG.Domain/Auth/Interfaces/IAuthenticator.cs
@@ -1,9 +1,11 @@
-﻿using System.Security.Claims;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace Project_ZIwG.Domain.Auth.Interfaces
 {
     public interface IAuthenticator
     {
         ClaimsPrincipal GetUserClaimsPrincipal(string username, string password);
+        JwtSecurityToken GetSecurityToken(string username, string password);
     }
 }
